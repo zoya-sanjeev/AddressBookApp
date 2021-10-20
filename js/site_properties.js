685fd6b63d077bf42163241e1609f9973db6b74f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-20 22:42:54 +0530

    ADDED: site properties for json server added

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
     home_page: "../pages/Home.html",
-    add_contact_page: "../pages/AddContact.html"
+    add_contact_page: "../pages/AddContact.html",
+    server_url: "http://127.0.0.1:3000/AddressBookDB/" ,
+    use_local_storage: "false"
 }
