b806a9f28391ac761445224e88c169f9d42e39e2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-20 23:31:37 +0530

    MODIFIED: remove function to handle deletion from server

--- js/Home.js
+++ js/Home.js
@@ -65,9 +65,20 @@ const createInnerHtml = () => {
                 .indexOf(contactData);
     console.log(index);
     contactList.splice(index, 1);
-    localStorage.setItem("ContactList",JSON.stringify(contactList));
-    document.querySelector(".address-count").textContent = contactList.length;
-    createInnerHtml();
+    if(site_properties.use_local_storage.match("true")){
+        localStorage.setItem("ContactList",JSON.stringify(contactList));
+        createInnerHtml();
+    }else{
+        const deleteURL = site_properties.server_url + contactData.id.toString();
+        makeServiceCall("DELETE", deleteURL, false)
+            .then(responseText =>{
+                document.querySelector(".address-count").textContent = contactList.length;
+                createInnerHtml();
+            })
+            .catch(error =>{
+                console.log("DELETE Error Status: " + JSON.stringify(error));
+            });
+    }
     
   }
 
